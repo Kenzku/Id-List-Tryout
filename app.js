c793968951f1cd80774997f86727f2f222b8e66c
Watke <wsd.2012.ken@gmail.com>
2013-04-22 14:45:54 +0300

    use index.html so that portable to other server that does not support EJS

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/index', function(req,res){
+    res.sendfile(__dirname + '/public/index.html');
+});
 app.get('/test', function(req,res){
     res.sendfile(__dirname + '/public/test/indextest.html');
 });
